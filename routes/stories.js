94bd3a20f654db7b1fa5c5530abad4de5e94f90f
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-09-22 11:55:33 -0400

    Added ability to get and delete story by id

--- routes/stories.js
+++ routes/stories.js
@@ -1,9 +1,12 @@
-// Imports
+// * Imports * //
 const express = require("express");
 const router = express.Router();
+// Model Import
 const Stories = require("../models/Stories");
 
-// Get Requests
+// * Get Requests * //
+
+// Get Stories
 router.get("/", async (req, res) => {
   try {
     // Limit response to 20 stories
@@ -18,7 +21,19 @@ router.get("/specific", (req, res) => {
   res.send("Specific Story");
 });
 
-// Post Request
+// Get Specific Story by ID
+router.get("/:storyId", async (req, res) => {
+  try {
+    const story = await Stories.findById(req.params.storyId);
+    res.json(story);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+// * Post Requests * //
+
+// Submits Story
 router.post("/", async (req, res) => {
   const story = new Stories({
     title: req.body.title,
@@ -32,6 +47,18 @@ router.post("/", async (req, res) => {
     res.status(200).json(savedStory);
   } catch (err) {
     res.status(500).json({ message: err });
+  }
+});
+
+// * Delete Requests * //
+
+// Delete Story by ID
+router.delete("/:storyId", async (req, res) => {
+  try {
+    const removedStory = await Stories.remove({ _id: req.params.storyId });
+    res.json(removedStory);
+  } catch (err) {
+    res.json({ message: err });
   }
 });
 
